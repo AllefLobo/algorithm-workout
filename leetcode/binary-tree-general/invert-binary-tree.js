4bab431c05e6ff8773471fbf5351f74c2f2511a9
Allef <allef.a.lobo@gmail.com>
2026-08-24 20:43:29 -0300

    Estudos de algoritmos (LeetCode: Invert Binary Tree)

--- leetcode/binary-tree-general/invert-binary-tree.js
+++ leetcode/binary-tree-general/invert-binary-tree.js
@@ -9,23 +9,17 @@ function TreeNode(val, left, right) {
  * @return {TreeNode}
  */
 var invertTree = function (root) {
-  console.log(root);
-  if (root === undefined) {
-    return root;
+  if (root === null) {
+    return null;
   }
-  let esq = root?.left;
-  let dir = root?.right;
-  !!esq ? (root.left = invertTree(dir)) : null;
-  !!dir ? (root.right = invertTree(esq)) : null;
+
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+
   return root;
 };
 
-// Input: root = [4,2,7,1,3,6,9]
-// Output: [4,7,2,9,6,3,1]
-let root = new TreeNode(
-  4,
-  new TreeNode(2, new TreeNode(1), new TreeNode(3)),
-  new TreeNode(7, new TreeNode(6), new TreeNode(9)),
-);
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+let root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
 
-console.log(invertTree(new TreeNode(1, new TreeNode(2))));
+console.log(invertTree(root));
